2b2de57036a513c584b6b14f0346ed7390b028da
Mark-4561 <74191452+Mark-4561@users.noreply.github.com>
2022-04-10 17:56:21 +0800

    test URL

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -5,9 +5,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from wxcloudrun.models import Counters
 
-
 logger = logging.getLogger('log')
-
 
 def index(request, _):
     """
@@ -89,3 +87,16 @@ def update_count(request):
     else:
         return JsonResponse({'code': -1, 'errorMsg': 'action参数错误'},
                     json_dumps_params={'ensure_ascii': False})
+
+def OCR(request):
+    
+    rsp = JsonResponse({'code': 0, 'errorMsg': ''}, json_dumps_params={'ensure_ascii': False})
+    if request.method == 'GET' or request.method == 'get':
+        rsp = get_count()
+    elif request.method == 'POST' or request.method == 'post':
+        rsp = update_count(request)
+    else:
+        rsp = JsonResponse({'code': -1, 'errorMsg': '请求方式错误'},
+                           json_dumps_params={'ensure_ascii': False})
+    logger.info('response result: {}'.format(rsp.content.decode('utf-8')))
+    return rsp
