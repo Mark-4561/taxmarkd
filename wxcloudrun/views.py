784d479131ae3c75d9e4fa2dd4ac87c4c4c0f07c
Mark-4561 <74191452+Mark-4561@users.noreply.github.com>
2022-04-11 20:06:51 +0800

    test try-1

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -4,6 +4,7 @@ import logging
 from django.http import JsonResponse
 from django.shortcuts import render
 from wxcloudrun.models import Counters
+from wxcloudrun.models import OCR
 
 logger = logging.getLogger('log')
 
@@ -130,28 +131,15 @@ def update_ocr(request):
     body_unicode = request.body.decode('utf-8')
     body = json.loads(body_unicode)
 
-    if 'action' not in body:
+    if 'file' not in body:
         return JsonResponse({'code': -1, 'errorMsg': '缺少action参数'},
                             json_dumps_params={'ensure_ascii': False})
 
-    if body['action'] == 'inc':
-        try:
-            data = Counters.objects.get(id=1)
-        except Counters.DoesNotExist:
-            data = Counters()
-        data.id = 1
-        data.count += 1
-        data.save()
-        return JsonResponse({'code': 0, "data": data.count},
+    if body['file'] == 'test.png':
+        data = OCR.start(body['file'])
+        return JsonResponse({'code': 0, "data": data},
                             json_dumps_params={'ensure_ascii': False})
-    elif body['action'] == 'clear':
-        try:
-            data = Counters.objects.get(id=1)
-            data.delete()
-        except Counters.DoesNotExist:
-            logger.info('record not exist')
-        return JsonResponse({'code': 0, 'data': 0},
-                            json_dumps_params={'ensure_ascii': False})
+    
     else:
         return JsonResponse({'code': -1, 'errorMsg': 'action参数错误'},
                             json_dumps_params={'ensure_ascii': False})
